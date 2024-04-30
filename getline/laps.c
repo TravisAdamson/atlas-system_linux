49c0d9dbb21a6860d867fb82b0ca2ff8307ac803
Travis Adamson <tndadamson@gmail.com>
2024-04-30 10:06:26 -0500

    More sorting

--- getline/laps.c
+++ getline/laps.c
@@ -107,8 +107,8 @@ void newest_cars(int id, cars_t **current_cars)
 		{
 			if (tmp_cars->id > id)
 			{
-				newest_car->next = tmp_cars->next;
 				tmp_cars->next = newest_car;
+				newest_car->next = tmp_cars;
 				break;
 			}
 			else if (tmp_cars->next == NULL)
