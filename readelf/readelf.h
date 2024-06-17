fcf90785cf9857ce0d888746261609bd02cdb356
Travis <travis.adamson@atlasschool.com>
2024-06-17 08:28:57 -0500

    Adding 0-readelf file in current state

--- readelf/readelf.h
+++ readelf/readelf.h
@@ -2,8 +2,13 @@
 #define __readelf_h__
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <elf.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
 #endif
