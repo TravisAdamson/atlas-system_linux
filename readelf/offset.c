c0ac6d539b9b6a51dd6ec3549f730d43d42d5c08
Travis <travis.adamson@atlasschool.com>
2024-06-18 10:28:40 -0500

    BE fixes

--- readelf/offset.c
+++ readelf/offset.c
@@ -4,6 +4,8 @@ void print_offset_32(Elf32_Ehdr *e_hdr)
 {
 	Elf32_Off sect_off = e_hdr->e_shoff;
 
+	if (e_hdr->e_ident[EI_DATA] == ELFDATA2MSB)
+    	sect_off = __bswap_32(e_hdr->e_shoff);
 	printf("%u (bytes into file)\n", sect_off);
 }
 
@@ -18,6 +20,8 @@ void print_poffset_32(Elf32_Ehdr *e_hdr)
 {
 	Elf32_Off prog_off = e_hdr->e_phoff;
 
+	if (e_hdr->e_ident[EI_DATA] == ELFDATA2MSB)
+    	prog_off = __bswap_32(e_hdr->e_phoff);
 	printf("%u (bytes into file)\n", prog_off);
 }
 
