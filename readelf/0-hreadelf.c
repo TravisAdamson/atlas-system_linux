fe52bd17bb938c3de61ca64dd434f8ef7bcfe5bf
Travis <travis.adamson@atlasschool.com>
2024-06-18 08:30:50 -0500

    Fixing spacing and formating for progam header start and section header start

--- readelf/0-hreadelf.c
+++ readelf/0-hreadelf.c
@@ -77,8 +77,8 @@ void print_head_32(Elf32_Ehdr *e_hdr)
 	print_machine_32(e_hdr);
 	printf(" %-35s0x%x\n", "Version:", e_hdr->e_version);
 	printf(" %-35s0x%x\n", "Entry point address:", e_hdr->e_entry);
-	printf(" %-35s%x (bytes into file)\n",
-		   "Start of program headers:", e_hdr->e_phoff);
+	printf(" %-35s", "Start of program headers:");
+	print_offset_32(e_hdr);
 	printf(" %-35s", "Start of section headers:");
 	print_offset_32(e_hdr);
 	printf(" %-35s0x%x\n", "Flags:", e_hdr->e_flags);
@@ -121,10 +121,10 @@ void print_head_64(Elf64_Ehdr *e_hdr)
 	print_machine_64(e_hdr);
 	printf(" %-35s0x%x\n", "Version:", e_hdr->e_version);
 	printf(" %-35s0x%lx\n", "Entry point address:", e_hdr->e_entry);
-	printf(" %-35s%lx (bytes into file)\n",
-		   "Start of program headers:", e_hdr->e_phoff);
-	printf(" %-35s%lx (bytes into file)\n",
-		   "Start of section headers:", e_hdr->e_shoff);
+	printf(" %-35s", "Start of program headers:");
+	print_offset_64(e_hdr);
+	printf(" %-35s", "Start of section headers:");
+	print_offset_64(e_hdr);
 	printf(" %-35s0x%x\n", "Flags:", e_hdr->e_flags);
 	printf(" %-35s%u (bytes)\n",
 		   "Size of this header:", e_hdr->e_ehsize);
--- readelf/offset.c
+++ readelf/offset.c
@@ -2,12 +2,12 @@
 
 void print_offset_32(Elf32_Ehdr *e_hdr)
 {
-    Elf32_Off sect_off = e_hdr->e_shoff;
-    printf(" %u (bytes into file)\n", sect_off);
+	Elf32_Off sect_off = e_hdr->e_shoff;
+	printf("%u (bytes into file)\n", sect_off);
 }
 
 void print_offset_64(Elf64_Ehdr *e_hdr)
 {
-    Elf64_Off sect_off =  e_hdr->e_shoff;
-    printf(" %lu (bytes into file)\n", sect_off);
+	Elf64_Off sect_off =  e_hdr->e_shoff;
+	printf("%lu (bytes into file)\n", sect_off);
 }
