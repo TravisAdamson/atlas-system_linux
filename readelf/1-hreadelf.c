bad4d6a66ec75e2f2120efe82df12cb3fa1bda61
Travis <travis.adamson@atlasschool.com>
2024-06-19 11:16:59 -0500

    Betty

--- readelf/1-hreadelf.c
+++ readelf/1-hreadelf.c
@@ -9,20 +9,20 @@
 
 int main(int argc, char **argv)
 {
-    void *maps;
-    size_t filesize;
+	void *maps;
+	size_t filesize;
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <elf-file>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <elf-file>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
 
-    maps = open_and_map_file(argv[1], &filesize);
-    if (!maps) {
-        return EXIT_FAILURE;
-    }
+	maps = open_and_map_file(argv[1], &filesize);
+	if (!maps)
+		return (EXIT_FAILURE);
 
-    process_elf(maps, filesize);
-    munmap(maps, filesize);
-    return EXIT_SUCCESS;
+	process_elf(maps, filesize);
+	munmap(maps, filesize);
+	return (EXIT_SUCCESS);
 }
