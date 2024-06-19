01655e6392b6d8e2a51b9c5a41fc6f5ab713cfa8
Travis <travis.adamson@atlasschool.com>
2024-06-19 11:10:58 -0500

    Forgot h file updates

--- readelf/1hreadelf.h
+++ readelf/1hreadelf.h
@@ -13,5 +13,12 @@
 
 void print_section_headers_32(Elf32_Ehdr *ehdr, Elf32_Shdr *shdr, const char *strtab);
 void print_section_headers_64(Elf64_Ehdr *ehdr, Elf64_Shdr *shdr, const char *strtab);
+void *open_and_map_file(const char *filename, size_t *filesize);
+void check_elf_magic(Elf64_Ehdr *ehdr);
+void process_elf(void *maps, size_t filesize);
+void process_elf64(Elf64_Ehdr *ehdr, int is_big_endian, void *maps);
+void process_elf32(Elf32_Ehdr *ehdr32, int is_big_endian, void *maps);
+void swap_endianess_64(Elf64_Shdr *shdr, int shnum);
+void swap_endianess_32(Elf32_Shdr *shdr, int shnum);
 
 #endif
