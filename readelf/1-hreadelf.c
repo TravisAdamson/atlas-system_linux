3a6646611f3a02eedd7b38cd0e92e343b5ed63d5
Travis <travis.adamson@atlasschool.com>
2024-06-19 10:36:22 -0500

    Formating

--- readelf/1-hreadelf.c
+++ readelf/1-hreadelf.c
@@ -16,7 +16,7 @@ void print_section_headers_32(Elf32_Ehdr *ehdr,
 
 	printf("There are %d section headers, starting at offset 0x%u:\n",
 		   ehdr->e_shnum, ehdr->e_shoff);
-	printf("[Nr] %-19s %-15s %-16s %-6s %-2s %-3s %-2s %-3s %-2s\n",
+	printf("[Nr] %-19s %-15s %-8s %-6s %-6s %-2s %-3s %-2s %-3s %-2s\n",
 		   "Name", "Type", "Addr", "Off", "Size",
 		   "ES", "Flg", "Lk", "Inf", "Al");
 	for (i = 0; i < ehdr->e_shnum; i++)
@@ -53,7 +53,7 @@ void print_section_headers_64(Elf64_Ehdr *ehdr,
 
 	printf("There are %d section headers, starting at offset 0x%lu:\n",
 		   ehdr->e_shnum, ehdr->e_shoff);
-	printf("[Nr] %-19s %-15s %-16s %-6s %-2s %-3s %-2s %-3s %-2s\n",
+	printf("[Nr] %-19s %-15s %-8s %-6s %-6s %-2s %-3s %-2s %-3s %-2s\n",
 		   "Name", "Type", "Addr", "Off", "Size",
 		   "ES", "Flg", "Lk", "Inf", "Al");
 	for (i = 0; i < ehdr->e_shnum; i++)
