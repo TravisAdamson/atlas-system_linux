5265782f15fd433544d35a5a3fb6fe5287c50e30
Travis <travis.adamson@atlasschool.com>
2024-06-25 06:03:32 -0500

    Task 1

---
+++ libasm/1-strcmp.asm
@@ -0,0 +1,64 @@
+BITS 64
+
+global asm_strcmp
+section .text
+	;; int asm_strcmp(const char *s1, const char *s2)
+	;; {
+	;; 	while (*s1 && *s2 && *s1 == *s2)
+	;;	{
+	;; 		s1++;
+	;; 		s2++;
+	;; 	}
+	;; 	if (*s1 > *s2)
+	;; 		return (1);
+	;; 	if (*s1 < *s2)
+	;; 		return (-1);
+	;;	return (0);
+	;; }
+asm_strcmp:
+	push rbp
+	mov rbp, rsp
+	mov QWORD [rbp - 8], rdi
+	mov QWORD [rbp - 16], rsi
+	jmp .test_s1_v_s2
+.strings_to_cmp:
+	add QWORD [rbp - 8], 1
+	add QWORD [rbp - 16], 1
+.test_s1_v_s2:
+	mov rax, QWORD [rbp - 8]
+	movzx eax, BYTE [rax]
+	test al, al
+	je .test_s1_gt_s2
+	mov rax, QWORD [rbp - 16]
+	movzx eax, BYTE [rax]
+	test al, al
+	je .test_s1_gt_s2
+	mov rax, QWORD [rbp - 8]
+	movzx edx, BYTE [rax]
+	mov rax, QWORD [rbp - 16]
+	movzx eax, BYTE [rax]
+	cmp dl, al
+	je .strings_to_cmp
+.test_s1_gt_s2:
+	mov rax, QWORD [rbp - 8]
+	movzx edx, BYTE [rax]
+	mov rax, QWORD [rbp - 16]
+	movzx eax, BYTE [rax]
+	cmp dl, al
+	jle .test_s1_lt_s2
+	mov eax, 1
+	jmp .return
+.test_s1_lt_s2:
+	mov rax, QWORD [rbp - 8]
+	movzx edx, BYTE [rax]
+	mov rax, QWORD [rbp - 16]
+	movzx eax, BYTE [rax]
+	cmp dl, al
+	jge .return_0
+	mov eax, -1
+	jmp .return
+.return_0:
+	mov eax, 0
+.return:
+	pop rbp
+	ret
--- libasm/libasm.h
+++ libasm/libasm.h
@@ -5,4 +5,6 @@
 
 size_t asm_strlen(const char *str);
 
+int asm_strcmp(const char *s1, const char *s2);
+
 #endif
