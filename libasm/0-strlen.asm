821dea27f6f468e2570b083801469383d2296f15
Travis <travis.adamson@atlasschool.com>
2024-06-25 05:39:22 -0500

    Task 0

---
+++ libasm/0-strlen.asm
@@ -0,0 +1,30 @@
+BITS 64
+
+global asm_strlen
+
+section .text
+	;; size_t asm_strlen(const char *str)
+	;; {
+	;;         size_t len = 0;
+	;;         for (; *str; str++)
+	;;                 len++;
+	;;         return (len);
+	;; }
+
+asm_strlen:
+	push rbp
+	mov rbp, rsp
+	mov QWORD [rbp - 24], rdi
+	mov QWORD [rbp - 8], 0
+	jmp .test_str_null
+.inc_str_len:
+	add QWORD [rbp - 8], 1
+	add QWORD [rbp - 24], 1
+.test_str_null:
+	mov rax, QWORD [rbp - 24]
+	movzx eax, BYTE [rax]
+	test al, al
+	jne .inc_str_len
+	mov rax, QWORD [rbp - 8]
+	pop rbp
+	ret
