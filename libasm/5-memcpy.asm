40b4fa72e856b651116ae6b42edf94e685a2fa9c
Travis <travis.adamson@atlasschool.com>
2024-06-27 07:13:48 -0500

    Testing a diff version

--- libasm/5-memcpy.asm
+++ libasm/5-memcpy.asm
@@ -9,12 +9,10 @@ asm_memcpy:
     mov rdi, rdi
     mov rsi, rsi
     mov rdx, rdx
-    test rdi, rdi
-    je .return_dest
-    test rsi, rsi
-    je .return_dest
+    test rdx, rdx
+    jz .return_dest
     mov rcx, rdx
-	rep movsb
+    rep movsb
 .return_dest:
     mov rax, rdi
     leave
