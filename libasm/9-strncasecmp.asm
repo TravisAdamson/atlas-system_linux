682266ecb2133905bf5da3d1446a0f88007e363c
Travis <travis.adamson@atlasschool.com>
2024-06-28 15:45:41 -0500

    Adding in the n char

---
+++ libasm/9-strncasecmp.asm
@@ -0,0 +1,66 @@
+BITS 64
+
+global asm_strncasecmp
+section .text
+	;; int asm_strcasecmp(const char *s1, const char *s2)
+	;; load stack, get individual chars to cmp,
+	;; convert to lower case if needed
+	;; compare, if not equal, return 1, or -1 as needed,
+	;; if equal continue, if both reach  null then return 0
+asm_strcasecmp:
+	push rbp
+	mov rbp, rsp
+	xor rax, rax
+	push rbx
+	push rcx
+
+.get_next_char:
+	cmp rdx, 0
+	jz .not_equals
+	movzx ebx, BYTE [rdi]
+	movzx ecx, BYTE [rsi]
+
+.check_null:
+	cmp bl, 0x00
+	jnz .remove_char_1_case
+	cmp cl, 0x00
+	jnz .remove_char_2_case
+	jmp .not_equals
+
+.remove_char_1_case:
+	cmp bl, 65
+	jl .remove_char_2_case
+	cmp bl, 90
+	jg .remove_char_2_case
+	add bx, 32
+	jmp .remove_char_2_case
+
+.remove_char_2_case:
+	cmp cl, 65
+	jl .char_1_v_char_2
+	cmp cl, 90
+	jg .char_1_v_char_2
+	add cx, 32
+	jmp .char_1_v_char_2
+
+.char_1_v_char_2:
+	cmp bl, cl
+	jnz .not_equals
+	inc rdi
+	inc rsi
+	dec rdx
+	jmp .get_next_char
+
+.not_equals:
+	sub rbx, rcx
+	mov rax, rbx
+	jmp .exit
+
+.return_found:
+	xor rax, rax
+
+.exit:
+	pop rcx
+	pop rbx
+	pop rbp
+	ret
--- libasm/libasm.h
+++ libasm/libasm.h
@@ -21,4 +21,6 @@ size_t asm_puts(const char *str);
 
 int asm_strcasecmp(const char *s1, const char *s2);
 
+int asm_strncasecmp(const char *s1, const char *s2, size_t n);
+
 #endif
