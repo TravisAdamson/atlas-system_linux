de31acc329d10bb6b20733ac7e2afa890a231322
Travis <travis.adamson@atlasschool.com>
2024-06-28 05:29:35 -0500

    Working with puts

---
+++ libasm/7-puts.asm
@@ -0,0 +1,30 @@
+BITS 64
+
+global asm_puts
+extern asm_strlen
+
+section .text
+	;; size_t asm_puts(const char *str)
+	;; load stack, call strlen, setup rax value for ret
+
+asm_puts:
+	push rbp
+	mov rbp, rsp
+	push rsi
+	push rdx
+	mov rsi, rdi
+
+.get_length:
+	call asm_strlen
+
+.setup_ret:
+	mov rdi, 1
+	mov rdx, rax
+	mov rax, 1
+
+.syscall_and_exit:
+	SYSCALL
+	pop rbp
+	pop rdx
+	pop rbp
+	ret
--- libasm/libasm.h
+++ libasm/libasm.h
@@ -17,4 +17,6 @@ void *asm_memcpy(void *dest, const void *src, size_t n);
 
 size_t asm_putc(int c);
 
+size_t asm_puts(const char *str);
+
 #endif
