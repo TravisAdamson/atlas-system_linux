421bbeeed74eec03cb3476ce257e5905bf01937a
Travis <travis.adamson@atlasschool.com>
2024-07-01 08:54:49 -0500

    Libasm

--- libasm/libasm.h
+++ libasm/libasm.h
@@ -23,4 +23,6 @@ int asm_strcasecmp(const char *s1, const char *s2);
 
 int asm_strncasecmp(const char *s1, const char *s2, size_t n);
 
+size_t asm_strspn(const char *s, const char *accept);
+
 #endif
