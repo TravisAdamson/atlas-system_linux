1ff3411c3bf8ca6d5aed48f0a0c4587814958d8b
Travis <travis.adamson@atlasschool.com>
2024-07-02 10:35:53 -0500

    2hreadelf.h

--- readelf/2hreadelf.h
+++ readelf/2hreadelf.h
@@ -18,10 +18,10 @@ void print_program_headers_64(Elf64_Ehdr * ehdr,
 							int is_big_endian);
 
 void print_program_headers_32(Elf32_Ehdr *ehdr32,
-                            Elf32_Phdr *phdr32,
-                            Elf32_Shdr *shdr32,
-                            const char *maps,
-                            int is_big_endian);
+							Elf32_Phdr *phdr32,
+							Elf32_Shdr *shdr32,
+							const char *maps,
+							int is_big_endian);
 
 void swap_endianess_64(Elf64_Phdr *phdr, int phnum);
 
@@ -35,17 +35,20 @@ const char *get_file_type(uint16_t e_type);
 const char *get_segment_type(uint32_t p_type);
 void initialize_segment_sections(char *segment_sections[], int num_sections);
 void print_segment_mapping_64(Elf64_Ehdr *ehdr, char *segment_sections[]);
-void map_sections_to_segments_64(Elf64_Ehdr *ehdr, Elf64_Phdr *phdr, Elf64_Shdr *shdr,
-							const char *shstrtab, char *segment_sections[]);
+void map_sections_to_segments_64(Elf64_Ehdr *ehdr, Elf64_Phdr *phdr,
+							Elf64_Shdr *shdr, const char *shstrtab,
+							char *segment_sections[]);
 void print_top_section_64(Elf64_Ehdr *ehdr,
 							Elf64_Phdr *phdr,
 							const char *maps);
 
 
 void print_program_headers_info_32(Elf32_Ehdr *ehdr32);
-void print_program_headers_32_2(Elf32_Phdr *phdr32, int e_phnum, const char *maps);
-void map_sections_to_segments_32(char **segment_sections, Elf32_Phdr *phdr32, int e_phnum,
-                              Elf32_Shdr *shdr32, const char *shstrtab, Elf32_Ehdr *ehdr32);
+void print_program_headers_32_2(Elf32_Phdr *phdr32, int e_phnum,
+							const char *maps);
+void map_sections_to_segments_32(char **segment_sections, Elf32_Phdr *phdr32,
+							int e_phnum, Elf32_Shdr *shdr32,
+							const char *shstrtab, Elf32_Ehdr *ehdr32);
 void print_segment_mapping_32(char **segment_sections, int e_phnum);
 
 #endif
