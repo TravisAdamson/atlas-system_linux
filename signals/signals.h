6eaa65ecb1b92385f3d7139bb3e00cc6754c28c6
Travis <travis.adamson@atlasschool.com>
2024-07-05 10:01:18 -0500

    Setting up header file

---
+++ signals/signals.h
@@ -0,0 +1,12 @@
+#ifndef __SIGNALS_H_
+#define __SIGNALS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+
+int handle_signal(void);
+
+#endif
