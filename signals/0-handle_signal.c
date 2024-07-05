0c69976614bc0b6de785e7fc6adcda3a071ad9c1
Travis <travis.adamson@atlasschool.com>
2024-07-05 10:09:07 -0500

    Task 0

---
+++ signals/0-handle_signal.c
@@ -0,0 +1,25 @@
+#include "signals.h"
+
+/**
+ * sigint_processor - Action to be taken on SIGINT
+ * @sig: Incoming signal
+*/
+
+void sigint_processor(int sig)
+{
+	printf("Gotcha! [%d]\n", sig);
+	fflush(stdout);
+}
+
+/**
+ * handle_signal - function for handling SIGINT
+ * Return: 0 on success, -1 on failure
+*/
+
+int handle_signal(void)
+{
+	if (signal(SIGINT, sigint_processor) == SIG_ERR)
+		return (-1);
+	else
+		return(0);
+}
