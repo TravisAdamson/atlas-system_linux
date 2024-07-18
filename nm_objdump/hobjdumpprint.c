baca6a92c7be46e3606fd12f09d4baf217f3c29a
Travis <travis.adamson@atlasschool.com>
2024-07-18 10:16:59 -0500

    Testing

--- nm_objdump/hobjdumpprint.c
+++ nm_objdump/hobjdumpprint.c
@@ -42,7 +42,14 @@ void print_section_contents_32(Elf32_Shdr *section,
 		printf("Contents of section %s:\n", section_name);
 	for (i = 0; i < section->sh_size; i += 16)
 	{
-		printf("%8lx ", (unsigned long)(section->sh_addr + i));
+		if ((section->sh_addr + i) > 0x1000)
+		{
+    		printf("%08lx ", (unsigned long)(section->sh_addr + i));
+		}
+		else
+    	{
+			printf("%8lx ", (unsigned long)(section->sh_addr + i));
+		}
 		for (j = 0; j < 16; ++j)
 		{
 			if (i + j < section->sh_size)
