7e5294d58458a788a5b0cefb78510a44e08fcbf0
Travis <travis.adamson@atlasschool.com>
2024-07-18 13:47:06 -0500

    yeah

--- nm_objdump/hobjdumpprint.c
+++ nm_objdump/hobjdumpprint.c
@@ -106,8 +106,8 @@ void print_section_contents_64(Elf64_Shdr *section,
 		printf("Contents of section %s:\n", section_name);
 	for (i = 0; i < section->sh_size; i += 16)
 	{
-		if ((section->sh_addr + i) > 0xf000 && (section->sh_addr + i) < 0xffff)
-			printf(" %0lx ", (unsigned long)(section->sh_addr + i));
+		if ((section->sh_addr + i) > 0xf1c0 && (section->sh_addr + i) < 0xffff)
+			printf(" %05lx ", (unsigned long)(section->sh_addr + i));
 		else if ((section->sh_addr + i) > 0x1000)
 			printf(" %lx ", (unsigned long)(section->sh_addr + i));
 		else
