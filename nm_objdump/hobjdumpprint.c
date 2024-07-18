fba124c51cc58b0355285ec7f73ba744170302bf
Travis <travis.adamson@atlasschool.com>
2024-07-18 11:52:11 -0500

    Stuff

--- nm_objdump/hobjdumpprint.c
+++ nm_objdump/hobjdumpprint.c
@@ -40,8 +40,7 @@ int print_32(Elf32_Ehdr *ehdr, Elf32_Shdr *shdr, const unsigned char *maps,
 			(strcmp((const char *)section_name, ".tm_clone_table")) &&
 			(strcmp((const char *)section_name, ".rel.text")) &&
 			(strcmp((const char *)section_name, ".rel.data")) &&
-			(strcmp((const char *)section_name, ".rela.text.startup")) &&
-			(strcmp((const char *)section_name, ".rela.debug.info")))
+			(strncmp((const char *)section_name, ".rela", 5)))
 			if (section->sh_size > 0)
 				print_section_contents_32(section, section_name, maps);
 	}
@@ -165,7 +164,7 @@ int print_64(Elf64_Ehdr *ehdr, Elf64_Shdr *shdr, const unsigned char *maps,
 			(strcmp((const char *)section_name, ".rel.text")) &&
 			(strcmp((const char *)section_name, ".rel.data")) &&
 			(strcmp((const char *)section_name, ".rela.text.startup")) &&
-			(strcmp((const char *)section_name, ".rela.debug.info")))
+			(strncmp((const char *)section_name, ".rela", 5)))
 			if (section->sh_size > 0)
 				print_section_contents_64(section, section_name, maps);
 	}
