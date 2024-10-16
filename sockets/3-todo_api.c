e554897f5dcf4749e8e01dceb8d15ae5e6ab4d31
Travis <travis.adamson@atlasschool.com>
2024-10-16 08:49:11 -0500

    Todo 3

--- sockets/3-todo_api.c
+++ sockets/3-todo_api.c
@@ -9,6 +9,7 @@
 
 #define MESSAGE "HTTP/1.1 200 OK\r\n\r\n"
 void query_parser(char *query);
+void body_parser(char *query);
 
 /**
  * main - Socket API
@@ -18,7 +19,7 @@ int main(void)
 {
 	int socket_fd, connect;
 	size_t bytes = 0;
-	char buffer[4096], sent[32] = MESSAGE;
+	char buffer[4096], path[50], sent[32] = MESSAGE;
 	struct sockaddr_in s_address;
 	socklen_t addrlen = sizeof(s_address);
 
@@ -42,12 +43,32 @@ int main(void)
 		if (bytes > 0)
 		{
 			printf("Raw request: \"%s\"\n", buffer), fflush(stdout);
-			query_parser(buffer);
+			sscanf(buffer, "%*s %s", path);
+			printf("Path: %s\n", path), fflush(stdout);
+			body_parser(buffer);
 		}
 		send(connect, sent, sizeof(sent), 0);
 		close(connect);
 	}
 	return (0);
+}
+
+/**
+ * body_parser - parse an http query
+ * @query: query string to parse
+*/
+void body_parser(char *query)
+{
+	int i = 0, flag = 0;
+	char *token = NULL, *lines[16] = {0}, *body = NULL;
+
+	do {
+		token = strsep(&query, "\r\n");
+		if (token)
+			lines[i++] = token, flag = 1;
+	} while (token && flag--);
+
+	body = lines[i - 1]; query_parser(body);
 }
 
 /**
@@ -57,17 +78,17 @@ int main(void)
 void query_parser(char *query)
 {
 	int i = 0, flag = 0;
-	char *token = NULL, *headers[16] = {0}, key[50], val[50];
+	char *token = NULL, *key_vals[16] = {0}, key[50], val[50];
 
 	do {
-		token = strsep(&query, "\r\n");
-		if (token)
-			headers[i++] = token, flag = 1;
+		token = strsep(&query, "&");
+		if (token && token[0])
+			key_vals[i++] = token, flag = 1;
 	} while (token && flag--);
 
-	for (i = 1; headers[i]; i++)
+	for (i = 0; key_vals[i]; i++)
 	{
-		if (sscanf(headers[i],"%[^:]:%s", key, val) > 0)
-			printf("Header: \"%s\" -> \"%s\"\n", key, val), fflush(stdout);
+		sscanf(key_vals[i],"%[^=]=%s", key, val);
+		printf("Body param: \"%s\" -> \"%s\"\n", key, val), fflush(stdout);
 	}
 }
